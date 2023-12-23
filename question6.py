00df48bb3ead83b615b80c39150b2284b6bb3d0f
amaterassu <gennusoangelo17@gmail.com>
2023-12-23 09:39:33 +0100

    I really don't see how this fits

--- question6.py
+++ question6.py
@@ -23,8 +23,8 @@ sc.setLogLevel("ERROR")
 os.system("rm -rf ./results/question5")
 
 #Depends on the file, we load the CSV file
-wholeFile1 = sc.textFile("./data/task_events/*.csv")
-wholeFile2 = sc.textFile("./data/task_usage/*.csv")
+wholeFile1 = sc.textFile("./data/task_events/part-000*-of-00500.csv")
+wholeFile2 = sc.textFile("./data/task_usage/part-000*-of-00500.csv")
 
 #The first line of the file defines the name of each column in the cvs file
 #We store it as an array in the driver program
@@ -42,7 +42,19 @@ entries2 = wholeFile2.map(lambda x: x.split(','))
 
 table1 = entries1.filter(lambda x: x[9]!='').map(lambda x: ((x[2], x[3], x[4]), (x[9], x[10], x[11])))
 table2 = entries2.map(lambda x: ((x[2], x[3], x[4]), (x[5], x[6], x[12])))
-table = table1.join(table2).map(lambda x: (x[0], (x[1][0][0], x[1][0][1], x[1][0][2], x[1][1][0], x[1][1][1], x[1][1][2]))).sortBy(lambda x: x[1][0], ascending=False)
+table = table1.join(table2).map(lambda x: (x[0], (x[1][0][0], x[1][0][1], x[1][0][2], x[1][1][0], x[1][1][1], x[1][1][2]))).sortBy(lambda x: x[1][0], ascending=False).take(50)
+
+print(table)
+
+#print in a csv file as a table with the columns name: jobId, taskIndex, machineId, ReqCPU, ReqMem, ReqDisk, CPU, Mem, Disk
+
+with open("./results/question5.csv", "w") as f:
+    f.write("jobId,taskIndex,machineId,ReqCPU,ReqMem,ReqDisk,CPU,Mem,Disk\n")
+    for line in table:
+        f.write(line[0][0]+","+line[0][1]+","+line[0][2]+","+line[1][0]+","+line[1][1]+","+line[1][2]+","+line[1][3]+","+line[1][4]+","+line[1][5]+"\n")
+
+#save the RDD as a text file
+
 
 
 #keep the RDD in memory
